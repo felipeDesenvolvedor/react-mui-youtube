9e7f3df026a127bf5eaade9403c988d80fcecf98
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2023-01-10 11:57:39 -0300

    Adicionado toggle de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { ThemeProvider } from '@mui/material'
+
 import { BrowserRouter } from "react-router-dom";
 import {AppRoutes} from "./routes"
-import { LightTheme } from './shared/themes';
+import { AppThemeProvider } from './shared/contexts/index';
+
 
 function App() {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
     <BrowserRouter>
       <AppRoutes/>
     </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 }
 
